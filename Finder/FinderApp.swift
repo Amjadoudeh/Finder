5554861e31942ce5ff69bb2214ad434762094e79
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-20 10:49:29 +0200

    first commit

---
+++ Finder/FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderApp.swift
+//  Finder
+//
+//  Created by Amjad Oudeh on 20.09.22.
+//
+
+import SwiftUI
+
+@main
+struct FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
