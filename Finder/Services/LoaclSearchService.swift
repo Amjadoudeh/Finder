604f889a48a3c802d3b66d09d1683e974cb6eccc
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-20 12:00:23 +0200

    Create a search function

--- Finder/Services/LoaclSearchService.swift
+++ Finder/Services/LoaclSearchService.swift
@@ -13,4 +13,21 @@ class LocalSearchService: ObservableObject {
         locationManager.$region.assign(to: \.region, on: self)
             .store(in: &cancellables)
     }
+    
+    func search(query: String) {
+        ///A utility object for initiating map-based searches and processing the results.
+        let requset = MKLocalSearch.Request()
+        /// A string containing the desired search item.
+        requset.naturalLanguageQuery = query
+        /// we only want to provide data for this region
+        requset.region = locationManager.region
+        
+        /// Create a search request
+        let search = MKLocalSearch(request: requset)
+        search.start { response , error in
+            if let response = response {
+                let mapItems = response.mapItems
+            }
+        }
+    }
 }
